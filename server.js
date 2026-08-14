432b765ddd95ea299446a8c5a1d72b3c3023b29a
VITRUS-twt <contact@vitrushk.com>
2026-08-14 14:50:19 +0800

    Anthony commit

--- server.js
+++ server.js
@@ -7,13 +7,13 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
 
 // 中間件設定
 app.use(cors());
 app.use(express.json());
 
-// 靜態檔案託管（可以直接在 http://localhost:3000 存取你的 HTML/CSS/JS）
+// 靜態檔案託管（可以直接在 http://localhost:4000 存取你的 HTML/CSS/JS）
 app.use(express.static(path.join(__dirname)));
 
 // 匯入你剛改好的 API 邏輯
